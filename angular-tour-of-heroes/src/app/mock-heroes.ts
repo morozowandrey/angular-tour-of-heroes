e36075efd290667e00825885fc8fb5fbd57b6a49
Andrew Morozow <dedal@macbooks-MacBook-Pro.local>
2018-09-10 11:14:49 +0300

    services add

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,36 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {
+        id: 1,
+        name: 'Ushchlove'
+    },
+    {
+        id: 2,
+        name: 'Corona with Lime'
+    },
+    {
+        id: 3,
+        name: 'Dedal'
+    },
+    {
+        id: 4,
+        name: 'Oslo'
+    },
+    {
+        id: 5,
+        name: 'Bird'
+    },
+    {
+        id: 6,
+        name: 'Wants Fun'
+    },
+    {
+        id: 7,
+        name: 'Kajote'
+    },
+    {
+        id: 8,
+        name: 'Hell Drummy'
+    }
+]
